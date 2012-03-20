3485af261f0d6742685d2dcd70d0c8fd8098215b
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-03-20 11:57:05 -0400

    Factorial.

---
+++ fac.erl
@@ -0,0 +1,11 @@
+-module(fac).
+-export([fac/1,main/1]).
+
+main([A]) -> 
+    I = list_to_integer(atom_to_list(A)),
+    F = fac(I), 
+    io:format("factorial ~w = ~w~n", [I, F]),
+    init:stop().
+
+fac(0) -> 1;
+fac(N) -> N * fac(N-1).
