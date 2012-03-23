3c46539723ee908711143609f1be9322ca477652
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-03-22 23:49:29 -0400

    Useless module.

---
+++ useless.erl
@@ -0,0 +1,13 @@
+-module(useless).
+
+-export([add/2, hello/0, greet_and_add_two/1]).
+
+add(A, B) -> 
+    A + B.
+
+hello() -> 
+    io:format("Hello, world!").
+
+greet_and_add_two(X) -> 
+    hello(), 
+    add(X, 2).
